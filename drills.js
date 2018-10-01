6b54ef3eb78a39deb5ce1b50bd1c8afb1295e430
john <johnjacobcosgrove@gmail.com>
2018-10-01 12:25:49 -0400

    added reversing strings

--- drills.js
+++ drills.js
@@ -10,13 +10,23 @@
 
 // sheepCount(3);
 
-function arrayDouble(array){
-  if(!array.length){
-    return [];
+// function arrayDouble(array){
+//   if(!array.length){
+//     return [];
+//   }
+// //   const newValue = array[0] * 2;
+// //   return newValue + arrayDouble(array.slice(1));
+//   return [(array[0] *2), ...arrayDouble(array.slice(1))];
+// }
+
+// console.log(arrayDouble([2,3,4]));
+
+function reverseString(string){
+  if(string === ''){
+    return ''
   }
-//   const newValue = array[0] * 2;
-//   return newValue + arrayDouble(array.slice(1));
-  return [(array[0] *2), ...arrayDouble(array.slice(1))];
+  let newStr = string[string.length-1]
+  return newStr + reverseString(string.slice(0,-1))
 }
 
-console.log(arrayDouble([2,3,4]));
+console.log(reverseString('hello'))
